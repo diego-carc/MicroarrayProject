63f683d354b197a2f55d0dda67a47882ae0dbd90
Diego Carmona Campos <diegocar@lcg.unam.mx>
2024-03-03 20:32:25 -0600

    Added affyParser.R

---
+++ bin/parseAffy.R
@@ -0,0 +1,11 @@
+#!/usr/bin/env Rscript
+#' ---
+#' Rscript definition of the parse method for the python AffymetrixParser class
+#' ---
+
+# Load packages
+library(optparse)
+library(affy)
+library(makecdfenv)
+
+# Parse args
