785a4756b75a299480643b9c50555b91331c0057
Diego Carmona Campos <diegocar@lcg.unam.mx>
2024-03-28 06:03:36 -0600

    Added GenPix Parser

--- bin/utils_normalization.py
+++ bin/utils_normalization.py
@@ -2,7 +2,6 @@
 """
 import pandas as pd
 import numpy as np
-import re
 import logging
 from sys import stdout
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='[%m/%d/%Y %I:%M:%S %p]', level=logging.INFO, stream=stdout)
@@ -206,9 +205,116 @@ class NimbleGen(Microarray):
         # Mean redundancies and rename
         return rawData.groupby(annot).mean().rename(columns=lambda col: self.GSM)
 
-class GenePix(Microarray):
-    def __init__(self, dataFilePath, annotFilePath):
-        super().__init__(dataFilePath, annotFilePath)
+class GenPix(Microarray):
+    from pandas.core.api import DataFrame as DataFrame
+
+    def __init__(self, dataFilePath: str, annotFilePath: str = '', GSM: str = '', oneChannel: bool = True):
+        """Initiate a GenPix Microarray
+
+        _extended summary_[#_unique ID_]_
+
+        .. math:: _LaTeX formula_
+
+        Arguments:
+            dataFilePath (str): _description_
+            annotFilePath (str): _description_
+            GSM (str): _description_. Defaults to ''.
+            oneChannel (bool): _description_. Defaults to True.
+            annotCols (set): _description_. Defaults to set().
+            dataCols (set): _description_. Defaults to set().
+
+        References:
+            .. [#_unique ID_] _pubmed abbr journal title_ _vol_:_page or e-article id_ (_year_) https://doi.org/_doi_
+            .. [#_unique ID_] _first-author first-name last-name_ _book title_ (_year_) ISBN:_ISBN_ _http link_
+            .. [#_unique ID_] _article title_ _conference_ (_year_) _http link_"""
+        
+        # Call superclass init
+        logging.debug("Initiation GenPix")
+        super().__init__(dataFilePath, annotFilePath, GSM, oneChannel,
+                          annotCols = {"Name", '"Name"', "ID", '"ID"'},
+                        dataCols = {"F532 Median", "F635 Median", '"F532 Median"', '"F635 Median"'})
+        logging.debug("GenPix Initiation succesflu!")
+    
+    def parseRawData(self) -> DataFrame:
+        """ Parse raw gpr data files
+
+        _extended summary_[#_unique ID_]_
+
+        .. math:: _LaTeX formula_
+
+        Raises:
+            MicroarrayFileFormatError: _description_
+
+        Returns:
+            DataFrame: _description_
+
+        References:
+            .. [#_unique ID_] _pubmed abbr journal title_ _vol_:_page or e-article id_ (_year_) https://doi.org/_doi_
+            .. [#_unique ID_] _first-author first-name last-name_ _book title_ (_year_) ISBN:_ISBN_ _http link_
+            .. [#_unique ID_] _article title_ _conference_ (_year_) _http link_"""
+
+        # Check if file has GPR format
+        if self.dataFile.readChars(3) != "ATF":
+            raise MicroarrayFileFormatError(f"{self.dataFile.filePath} might not be a GenPix File")
+        
+        # Count comment lines
+        logging.debug("Counting comment lines")
+        commLines = self.countCommentLines()
+        logging.debug(f"{self.GSM} has {commLines} comment lines")
+
+        # Parse table
+        logging.debug(f"Parsing {self.GSM}")
+        rawData =  super().parseRawData(skiprows=range(commLines))
+        logging.debug(f"File parsed. Dim: {rawData.shape}")
+
+        # Rename dyes
+        rawData = rawData.rename(columns=lambda col: col.replace("647", "635").replace("555", "532"))
+        logging.debug("Colums renamed to use F635 and F532 nomenclature")
+
+        # Select columns
+        rawData = rawData[list(set(rawData.columns) & (self.annotCols | self.dataCols))]
+        logging.debug(f"Columns selected: {rawData.columns}")
+
+        # Select annotation column
+        ## Use column with the less number of NaN
+        annotColsPresent = self.annotCols & set(rawData.columns)
+        annot =  sorted(annotColsPresent, key=lambda col: sum(rawData[col].isna()))[0]
+        rawData = rawData.drop(annotColsPresent - {annot},axis=1)
+        logging.debug(f"Annotation col selected: {annot}")
+
+        # Mean and Rename
+        chName = lambda col : f"{self.GSM}_g" if "532" in col else f"{self.GSM}_r"
+        rawData = rawData.groupby(annot).mean().rename(columns=lambda col : self.GSM if (self.oneChannel) else chName(col))
+        logging.debug("Redundancies meaned and columns renamed")
+
+        return rawData
+    
+    def countCommentLines(self) -> int:
+        """ Count comment lines to skip
+
+        _extended summary_[#_unique ID_]_
+
+        .. math:: _LaTeX formula_
+
+        Returns:
+            int: _description_
+
+        References:
+            .. [#_unique ID_] _pubmed abbr journal title_ _vol_:_page or e-article id_ (_year_) https://doi.org/_doi_
+            .. [#_unique ID_] _first-author first-name last-name_ _book title_ (_year_) ISBN:_ISBN_ _http link_
+            .. [#_unique ID_] _article title_ _conference_ (_year_) _http link_"""
+        try: 
+            with open(self.dataFile.filePath) as file:
+                temp = file.readlines()
+        except UnicodeDecodeError: 
+            with open(self.dataFile.filePath, encoding="latin-1") as file:
+                temp = file.readlines()
+        finally:
+            # Check if line has intersection with annotation columns
+            for i, line in enumerate(temp):
+                if self.annotCols & set(line.split("\t")): return i
+            return -1
+
 
 class MicroarrayBatch():
     import numpy as np
