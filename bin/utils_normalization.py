1a7b431f843dc7f4a34fcddd1411d2316a597f9c
Diego Carmona Campos <diegocar@lcg.unam.mx>
2024-03-28 05:58:16 -0600

    Working on parsers

--- bin/utils_normalization.py
+++ bin/utils_normalization.py
@@ -1,5 +1,4 @@
 """
-
 """
 import pandas as pd
 import numpy as np
@@ -7,6 +6,7 @@ import re
 import logging
 from sys import stdout
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='[%m/%d/%Y %I:%M:%S %p]', level=logging.INFO, stream=stdout)
+
 
 class ColumnNameError(Exception):
     pass
@@ -24,11 +24,14 @@ class Microarray():
     import os
     
     
-    def __init__(self, dataFilePath:str, annotFilePath:str, GSM:str=None,
-                 oneChannel:bool=True, annotCols:set={}, dataCols:set={}):
+    def __init__(self, dataFilePath:str, annotFilePath:str, GSM:str='',
+                 oneChannel:bool=True, annotCols:set=set(), dataCols:set=set()):
         
         # Sample identifier
-        self.GSM =  GSM if GSM else self.re.search(r"(GSM\d+)", dataFilePath).group()
+        if not GSM and not (gsm := self.re.search(r"(GSM\d+)", dataFilePath)):
+            raise MicroarrayFileFormatError(r"Please provide a GSM to identify the sample or\
+                                            supply a filename matching GSM\d+ pattern")
+        self.GSM =  GSM if GSM else gsm.group()
         logging.debug(f"Microarray sample name is: {self.GSM}")
 
         # Channel count
@@ -75,6 +78,9 @@ class Microarray():
         finally:
             logging.debug(f"{self.dataFile.filePath} parsed")
             return file
+    
+    def parseAnnotData(self):
+        pass
 
 class Agilent(Microarray):
     def __init__(self, dataFilePath:str, annotFilePath:str="", oneChannel:bool=True):
@@ -143,7 +149,7 @@ class Agilent(Microarray):
         pass
 
 class Affymetrix(Microarray):
-    def __init__(self, dataFilePath: str, annotFilePath: str, GSM: str = None, oneChannel: bool = True, annotCols: set = {}, dataCols: set = {}):
+    def __init__(self, dataFilePath: str, annotFilePath: str, GSM: str = '', oneChannel: bool = True, annotCols: set = {}, dataCols: set = {}):
         super().__init__(dataFilePath, annotFilePath, GSM, oneChannel, annotCols, dataCols)
     
 class NimbleGen(Microarray):
@@ -204,89 +210,17 @@ class GenePix(Microarray):
     def __init__(self, dataFilePath, annotFilePath):
         super().__init__(dataFilePath, annotFilePath)
 
-class Parser:
-
-    def __init__(self, filePath, probeCol, dataCol):
-        self.filePath = filePath
-        self.probeCol = probeCol
-        self.dataCol = dataCol
-        self.start = self.findLine() 
-        if self.start == None: raise ColumnNameError(f"{self.probeCol} and {self.dataCol} failed as column names")
-        self.GSM = re.search(r"(GSM\d+)", filePath).group()
-        self.data = self.parseFile()
-    
-    def parseFile(self):
-        """
-        Reads a tab separated file into a pandas DataFrame
-        """
-        try: 
-            data = pd.read_csv(self.filePath, sep='\t', skiprows=range(self.start), usecols=[self.probeCol, self.dataCol])
-        except: 
-            data = pd.read_csv(self.filePath, sep='\t', skiprows=range(self.start), usecols=[self.probeCol, self.dataCol], encoding="latin-1")
-
-        if data.dtypes[self.dataCol] == "O": data[self.dataCol] = data[self.dataCol].apply(lambda strVal: float(strVal.replace(',', '.')))
-
-        return self.meanRedundancies(data) 
-
-    def meanRedundancies(self, dataFrame):
-        """
-        Groups the data by the repeated ProbeNames and returns the mean
-
-        """
-        grouped = {pName : data[self.dataCol].mean() for pName, data in dataFrame.groupby(self.probeCol)}
-        grouped = pd.DataFrame.from_dict({"ProbeName":grouped.keys(), self.GSM:grouped.values()})
-        return grouped.set_index("ProbeName")
-    
-    def findLine(self, encoding="utf-8"):
-        """
-        Iterates over the lines of a tab separated file to find the header line
-        """
-        try:
-            with open(self.filePath, encoding=encoding) as file:
-                for i,line in enumerate(file.readlines()):
-                    if self.dataCol in line and self.probeCol in line:
-                        return i
-                return None
-        except: 
-            if encoding == "latin-1": raise EncodingError(f"Could not open {self.filePath}")
-            return(self.findLine("latin-1"))
-
-    
-class Affymetrix(Parser):
-    
-    def __init__(self, filePath, probeCol, dataCol):
-        super().__init__(filePath, probeCol, dataCol)
-
-    
-class oldNimbleGen(Parser):
-    
-    def __init__(self, filePath, probeCol = "PROBE_ID", dataCol="PM"):
-        super().__init__(filePath, probeCol, dataCol)
-    
-    def parseFile(self):
-        return super().parseFile()
-    
-class GenePix(Parser):
-
-    def __init__(self, filePath, probeCol, dataCol):
-        super().__init__(filePath, probeCol, dataCol)
-
-class Illumina(Parser):
-    pass
-
-class Miscelaneous(Parser):
-    pass
-
 class MicroarrayBatch():
+    import numpy as np
+
     
-    def quantile_normalize(self):
-        df = self
+    def quantile_normalize(self, df):
         """
         input: dataframe with numerical columns
         output: dataframe with quantile normalized values
         """
         df_sorted = pd.DataFrame(np.sort(df.values, axis=0), index=df.index, columns=df.columns)
         df_mean = df_sorted.mean(axis=1)
-        df_mean.index = np.arange(1, len(df_mean) + 1)
+        df_mean.index = self.np.arange(1, len(df_mean) + 1)
         df_qn =df.rank(method="min").stack().astype(int).map(df_mean).unstack()
         return(df_qn)
